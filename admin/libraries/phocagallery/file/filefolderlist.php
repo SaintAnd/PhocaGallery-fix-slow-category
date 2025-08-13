9d2d0bcd4bec7e16662a73c2379725a773a8e1b0
Andrew Gospodin <utunittinge@mail.ru>
2025-08-13 20:24:36 +0300

    Update filefolderlist.php
    Файловый менеджер медленный. ФИКС

--- admin/libraries/phocagallery/file/filefolderlist.php
+++ admin/libraries/phocagallery/file/filefolderlist.php
@@ -24,104 +24,75 @@ setlocale(LC_ALL, 'C.UTF-8', 'C');
 
 class PhocaGalleryFileFolderList
 {
-	public static function getList($small = 0, $medium = 0, $large = 0, $refreshUrl = '') {
-		static $list;
+public static function getList($refreshUrl = '') {
+    static $list;
 
-		$params				= ComponentHelper::getParams( 'com_phocagallery' );
-		$clean_thumbnails 	= $params->get( 'clean_thumbnails', 0 );
+    // Если список уже обработан, возвращаем его
+    if (is_array($list)) {
+        return $list;
+    }
 
-		// Only process the list once per request
-		if (is_array($list)) {
-			return $list;
-		}
+    // Получаем текущий путь из запроса
+    $current = JFactory::getApplication()->input->get('folder', '', 'path');
 
-		// Get current path from request
-		$current = Factory::getApplication()->input->get('folder', '', 'path');
+    // Если путь не определен, устанавливаем его как пустой
+    if ($current == 'undefined') {
+        $current = '';
+    }
 
-		// If undefined, set to empty
-		if ($current == 'undefined') {
-			$current = '';
-		}
+    // Получаем путь к изображениям
+    $path = PhocaGalleryPath::getPath();
+    $origPath = JPath::clean($path->image_abs . $current);
+    $origPathServer = str_replace('\\', '/', $path->image_abs);
 
-		//Get folder variables from Helper
-		$path = PhocaGalleryPath::getPath();
+    $images = array();
+    $folders = array();
 
-		// Initialize variables
-		if (strlen($current) > 0) {
-			$origPath = Path::clean($path->image_abs.$current);
-		} else {
-			$origPath = $path->image_abs;
-		}
-		$origPathServer = str_replace('\\', '/', $path->image_abs);
+    // Получаем список файлов и папок из текущего каталога
+    $fileList = JFolder::files($origPath);
+    $folderList = JFolder::folders($origPath, '', false, false, array(0 => 'thumbs'));
 
-		$images 	= array ();
-		$folders 	= array ();
+    // Сортируем файлы
+    if (is_array($fileList) && !empty($fileList)) {
+        natcasesort($fileList);
+    }
 
-		// Get the list of files and folders from the given folder
-		$fileList 		= Folder::files($origPath);
-		$folderList 	= Folder::folders($origPath, '', false, false, array(0 => 'thumbs'));
+    // Обработка файлов
+    if ($fileList !== false) {
+        foreach ($fileList as $file) {
+            $ext = strtolower(JFile::getExt($file));
+            if (in_array($ext, ['jpg', 'png', 'gif', 'jpeg', 'webp'])) {
+                if (JFile::exists($origPath . '/' . $file) && substr($file, 0, 1) != '.' && strtolower($file) !== 'index.html') {
+                    $fileNo = $current . "/" . $file;
+                    $fileThumb = PhocaGalleryFileThumbnail::getOrCreateThumbnail($fileNo, $refreshUrl);
 
-		if(is_array($fileList) && !empty($fileList)) {
-			natcasesort($fileList);
-		}
+                    $tmp = new JObject();
+                    $tmp->name = $fileThumb['name'];
+                    $tmp->nameno = $fileThumb['name_no'];
+                    $tmp->linkthumbnailpath = $fileThumb['thumb_name_m_no_rel'];
+                    $tmp->linkthumbnailpathabs = $fileThumb['thumb_name_m_no_abs'];
+                    $images[] = $tmp;
+                }
+            }
+        }
+    }
 
-		$field			= Factory::getApplication()->input->get('field');;
-		$refreshUrl 	= $refreshUrl . '&folder='.$current.'&field='.$field;
+    // Обработка папок
+    if ($folderList !== false) {
+        foreach ($folderList as $folder) {
+            $tmp = new JObject();
+            $tmp->name = basename($folder);
+            $tmp->path_with_name = str_replace('\\', '/', JPath::clean($origPath . '/' . $folder));
+            $tmp->path_without_name_relative = $path->image_abs . str_replace($origPathServer, '', $tmp->path_with_name);
+            $tmp->path_with_name_relative_no = str_replace($origPathServer, '', $tmp->path_with_name);
 
+            $folders[] = $tmp;
+        }
+    }
 
-		// Iterate over the files if they exist
-		//file - abc.img, file_no - folder/abc.img
-		if ($fileList !== false) {
-			foreach ($fileList as $file) {
-
-				$ext = strtolower(File::getExt($file));
-				// Don't display thumbnails from defined files (don't save them into a database)...
-				$dontCreateThumb	= PhocaGalleryFileThumbnail::dontCreateThumb($file);
-				if ($dontCreateThumb == 1) {
-					$ext = '';// WE USE $ext FOR NOT CREATE A THUMBNAIL CLAUSE
-				}
-				if ($ext == 'jpg' || $ext == 'png' || $ext == 'gif' || $ext == 'jpeg' || $ext == 'webp' || $ext == 'avif') {
-
-					if (File::exists($origPath. '/'. $file) && substr($file, 0, 1) != '.' && strtolower($file) !== 'index.html') {
-
-						//Create thumbnails small, medium, large
-						$fileNo			= $current . "/" . $file;
-						$fileThumb 		= PhocaGalleryFileThumbnail::getOrCreateThumbnail($fileNo, $refreshUrl, $small, $medium, $large);
-
-						$tmp 						= new CMSObject();
-						$tmp->name 					= $fileThumb['name'];
-						$tmp->nameno 				= $fileThumb['name_no'];
-						$tmp->linkthumbnailpath		= $fileThumb['thumb_name_m_no_rel'];
-						$tmp->linkthumbnailpathabs	= $fileThumb['thumb_name_m_no_abs'];
-						$images[] 					= $tmp;
-					}
-				}
-			}
-		}
-
-		//Clean Thumbs Folder if there are thumbnail files but not original file
-		if ($clean_thumbnails == 1) {
-			PhocaGalleryFileFolder::cleanThumbsFolder();
-		}
-		// - - - - - - - - - - - -
-
-		// Iterate over the folders if they exist
-
-		if ($folderList !== false) {
-			foreach ($folderList as $folder) {
-				$tmp 							= new CMSObject();
-				$tmp->name 						= basename($folder);
-				$tmp->path_with_name 			= str_replace('\\', '/', Path::clean($origPath . '/'. $folder));
-				$tmp->path_without_name_relative= $path->image_abs . str_replace($origPathServer, '', $tmp->path_with_name);
-				$tmp->path_with_name_relative_no= str_replace($origPathServer, '', $tmp->path_with_name);
-
-
-				$folders[] = $tmp;
-			}
-		}
-
-		$list = array('folders' => $folders, 'Images' => $images);
-		return $list;
+    $list = array('folders' => $folders, 'Images' => $images);
+    return $list;
 	}
 }
 ?>
+
